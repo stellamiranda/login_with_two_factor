dfd12e14e626e7d0938539d989a6779df94e6409
Stella Miranda <stellamariam@gmail.com>
2015-02-28 18:26:37 -0500

    set up devise

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+  	# Include default devise modules. Others available are:
+  	# :confirmable, :lockable, :timeoutable and :omniauthable
+  	validates :first_name, presence: true
+  	validates :last_name, presence: true
+  	validates :celphone, presence: true
+
+  	devise :database_authenticatable, :registerable,
+        :recoverable, :rememberable, :trackable, :validatable
+end
